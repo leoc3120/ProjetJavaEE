95a40c747208df4cfb9859ca50dfc5888bb5484d
leoc3120 <58136950+leoc3120@users.noreply.github.com>
2022-02-14 16:06:48 +0100

    Add files via upload

---
+++ ProjetJAVAEE/src/main/java/com/airhacks/TestServlet.java
@@ -0,0 +1,42 @@
+package com.airhacks;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet(name = "Servlet", value="/affiche")
+public class TestServlet extends HttpServlet {
+
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+        throws IOException, ServletException
+    { 
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        out.println("<html>");
+        out.println("<head>");
+
+	    String title = "bonjour leo";
+	    Date date = new Date();
+
+	    out.println("<title>" + title + "</title>");
+        out.println("</head>");
+        out.println("<body bgcolor=\"white\">");
+
+        out.println("<h1>" + title + " trkl?" + "</h1>");
+        out.println(date);
+        out.println("</body>");
+        out.println("</html>");
+    }
+
+}
