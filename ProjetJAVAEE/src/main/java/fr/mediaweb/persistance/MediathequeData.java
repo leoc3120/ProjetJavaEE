672c79cdd76b85ae60c3d6589bde51a0785e74ac
Jules DE ALMEIDA <jules.de-almeida@etu.u-paris.fr>
2022-03-04 17:19:14 +0100

    Actualisation

---
+++ ProjetJAVAEE/src/main/java/fr/mediaweb/persistance/MediathequeData.java
@@ -0,0 +1,193 @@
+package fr.mediaweb.persistance;
+
+import mediatek2022.Document;
+import mediatek2022.Mediatheque;
+import mediatek2022.PersistentMediatheque;
+import mediatek2022.Utilisateur;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MediathequeData implements PersistentMediatheque {
+
+	private static final String url = "jdbc:mysql://tijger.o2switch.net:3306/vmvo1438_mediaweb";
+	private static final String user = "vmvo1438_mediaweb";
+	private static final String password = "mediaweb4568";
+
+	static {
+		Mediatheque.getInstance().setData(new MediathequeData());
+	}
+
+	protected MediathequeData() {
+	}
+
+	public static Connection getConnection() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			return DriverManager.getConnection(url, user, password);
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+	public List<Document> tousLesDocumentsDisponibles() {
+		Connection conn = getConnection();
+		Statement stmt;
+		ResultSet res;
+		List<Document> documents = new ArrayList<>();
+		try {
+			stmt = conn.createStatement();
+
+			res = stmt.executeQuery(
+					"SELECT `id_d`, `type_d`, `emprunt_d`, `titre_d`, `auteur_d`, `options_d` FROM document;");
+
+			while (res.next())
+				documents.add(new MediathequeDocument(res.getInt(1), res.getString(2), res.getString(3),
+						res.getString(4), res.getString(5), res.getString(6)));
+
+			res.close();
+			stmt.close();
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return documents;
+	}
+
+	public Utilisateur getUser(String login, String password) {
+		Connection conn = getConnection();
+		PreparedStatement stmt;
+		ResultSet res;
+		MediathequeUtilisateur utilisateur = null;
+		try {
+			stmt = conn
+					.prepareStatement("SELECT `id_u`, `nom_u`, `type_u` FROM utilisateur WHERE `login`=? AND `mdp`=?;");
+
+			stmt.setString(1, login);
+			stmt.setString(2, password);
+
+			res = stmt.executeQuery();
+
+			utilisateur = res.next() ? new MediathequeUtilisateur(res.getInt(1), res.getString(2), res.getString(3))
+					: null;
+
+			res.close();
+			stmt.close();
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return utilisateur;
+	}
+
+	public Document getDocument(int numDocument) {
+		Connection conn = getConnection();
+		PreparedStatement stmt;
+		ResultSet res;
+		MediathequeDocument document = null;
+		try {
+			stmt = conn.prepareStatement(
+					"SELECT `id_d`, `type_d`, `emprunt_d`, `titre_d`, `auteur_d`, `options_d` FROM document WHERE `id_d`=?;");
+
+			stmt.setInt(1, numDocument);
+			res = stmt.executeQuery();
+
+			document = res.next()
+					? new MediathequeDocument(res.getInt(1), res.getString(2), res.getString(3), res.getString(4),
+							res.getString(5), res.getString(6))
+					: null;
+
+			res.close();
+			stmt.close();
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return document;
+	}
+
+	private Integer getUserID(String nomUtilisateur) {
+		Connection conn = getConnection();
+		PreparedStatement stmt;
+		ResultSet res;
+		Integer id = null;
+		try {
+			stmt = conn.prepareStatement("SELECT `id_u` FROM utilisateur WHERE `nom_u`=?;");
+
+			stmt.setString(1, nomUtilisateur);
+
+			res = stmt.executeQuery();
+
+			id = res.next() ? res.getInt(1) : null;
+
+			res.close();
+			stmt.close();
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return id;
+	}
+
+	public void ajoutDocument(int type, Object... args) {
+		Connection conn = getConnection();
+		PreparedStatement stmt;
+		try {
+			stmt = conn.prepareStatement("INSERT INTO document (`type_d`, `auteur_d`, `titre_d`) VALUES (?, ?, ?);");
+
+			stmt.setString(1, (String) args[0]);
+			stmt.setString(2, (String) args[1]);
+			stmt.setString(3, (String) args[2]);
+
+			stmt.execute();
+			stmt.close();
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public int emprunterDocument(int numDocument, String nomUtilisateur) {
+		int id_u = getUserID(nomUtilisateur);
+
+		Connection conn = getConnection();
+		PreparedStatement stmt;
+		try {
+			stmt = conn.prepareStatement("UPDATE document SET `emprunt_d`=? WHERE `id_d`=?;");
+
+			stmt.setInt(1, id_u);
+			stmt.setInt(2, numDocument);
+
+			stmt.execute();
+			stmt.close();
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return id_u;
+	}
+
+	public void retournerDocument(int numDocument) {
+		Connection conn = getConnection();
+		PreparedStatement stmt;
+		try {
+			stmt = conn.prepareStatement("UPDATE document SET `emprunt_d`=-1 WHERE `id_d`=?;");
+
+			stmt.setInt(1, numDocument);
+
+			stmt.execute();
+			stmt.close();
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
--- ProjetJAVAEE/src/main/java/fr/mediaweb/persistance/TestServlet.java
+++ ProjetJAVAEE/src/main/java/fr/mediaweb/persistance/TestServlet.java
@@ -1,13 +1,8 @@
 package fr.mediaweb.persistance;
 
-import mediatek2022.Document;
 import mediatek2022.Utilisateur;
 
 import java.io.IOException;
-import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -16,23 +11,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(name = "Servlet", value = "/affiche")
-public class TestServlet extends HttpServlet implements DataManager {
+public class TestServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
-	private static final String url = "jdbc:mysql://tijger.o2switch.net:3306/vmvo1438_mediaweb";
-    private static final String user = "vmvo1438_mediaweb";
-    private static final String password = "mediaweb4568";
 
-    public static Connection getConnection() {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            return DriverManager.getConnection(url, user, password);
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-        }
+	private MediathequeData Mediatheque = new MediathequeData();
 
-        return null;
-    }
     
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws IOException, ServletException {
@@ -40,147 +24,27 @@ public class TestServlet extends HttpServlet implements DataManager {
 		response.setContentType("text/html");
 		RequestDispatcher view;
 		Utilisateur u;
-		try {
-			u = getUser(request.getParameter("login"),request.getParameter("mdp"));
-			if(u != null) {
-				view = request.getRequestDispatcher("WEB-INF/template/pagep.jsp");
-				request.setAttribute("nomU", u.name());
-				for(int i=1; i<tousLesDocumentsDisponibles().size(); i++) {
-					if(i == 1) {
-						request.setAttribute("documents", tousLesDocumentsDisponibles().get(i).toString());
-					}
-					else {
-						request.setAttribute("documents", request.getAttribute("documents") 
-								+ tousLesDocumentsDisponibles().get(i).toString());
-					}
-				};
-						
-				view.forward(request, response);
-			}	
-		} catch (SQLException e) {
-			e.printStackTrace();
+		u = Mediatheque.getUser(request.getParameter("login"),request.getParameter("mdp"));
+		if(u != null) {
+			view = request.getRequestDispatcher("WEB-INF/template/pagep.jsp");
+			request.setAttribute("nomU", u.name());
+			for(int i=1; i<Mediatheque.tousLesDocumentsDisponibles().size(); i++) {
+				if(i == 1) {
+					request.setAttribute("documents", Mediatheque.tousLesDocumentsDisponibles().get(i).toString());
+				}
+				else {
+					request.setAttribute("documents", request.getAttribute("documents") 
+							+ Mediatheque.tousLesDocumentsDisponibles().get(i).toString());
+				}
+			};
+					
+			view.forward(request, response);
 		}
 
 		view = request.getRequestDispatcher("WEB-INF/template/accueil.jsp");
 		view.forward(request, response);
 	}
 
-    @Override
-    public List<Document> tousLesDocumentsDisponibles() throws SQLException {
-        Connection conn = getConnection();
-        Statement stmt = conn.createStatement();
-        ResultSet res = stmt.executeQuery("SELECT `id_d`, `type_d`, `emprunt_d`, `titre_d`, `auteur_d`, `options_d` FROM document;");
 
-        List<Document> documents = new ArrayList<>();
-        while (res.next()) documents.add(new MediathequeDocument(res.getInt(1), res.getString(2),
-                res.getString(3),
-                res.getString(4),
-                res.getString(5),
-                res.getString(6)));
-
-        res.close();
-        stmt.close();
-        conn.close();
-
-        return documents;
-    }
-
-    @Override
-    public Utilisateur getUser(String identifiant, String mdp) throws SQLException {
-        Connection conn = getConnection();
-        PreparedStatement stmt = conn.prepareStatement("SELECT `id_u`, `nom_u`, `type_u` FROM utilisateur WHERE `login`=? AND `mdp`=?;");
-        stmt.setString(1, identifiant);
-        stmt.setString(2, mdp);
-
-        ResultSet res = stmt.executeQuery();
-
-        MediathequeUtilisateur utilisateur = res.next() ? new MediathequeUtilisateur(res.getInt(1),
-                res.getString(2),
-                res.getString(3)) : null;
-
-        res.close();
-        stmt.close();
-        conn.close();
-
-        return utilisateur;
-    }
-
-    private Integer getUserID(String nomUtilisateur) throws SQLException {
-        Connection conn = getConnection();
-        PreparedStatement stmt = conn.prepareStatement("SELECT `id_u` FROM utilisateur WHERE `nom_u`=?;");
-        stmt.setString(1, nomUtilisateur);
-
-        ResultSet res = stmt.executeQuery();
-
-        Integer id = res.next() ? res.getInt(1) : null;
-
-        res.close();
-        stmt.close();
-        conn.close();
-
-        return id;
-    }
-
-    @Override
-    public Document getDocument(int numDocument) throws SQLException {
-        Connection conn = getConnection();
-        PreparedStatement stmt = conn.prepareStatement("SELECT `id_d`, `type_d`, `emprunt_d`, `titre_d`, `auteur_d`, `options_d` FROM document WHERE `id_d`=?;");
-        stmt.setInt(1, numDocument);
-
-        ResultSet res = stmt.executeQuery();
-
-        MediathequeDocument document = res.next() ? new MediathequeDocument(res.getInt(1),
-                res.getString(2),
-                res.getString(3),
-                res.getString(4),
-                res.getString(5),
-                res.getString(6)) : null;
-
-        res.close();
-        stmt.close();
-        conn.close();
-
-        return document;
-    }
-
-    @Override
-    public void ajoutDocument(int type, Object... args) throws SQLException {
-        Connection conn = getConnection();
-        PreparedStatement stmt = conn.prepareStatement("INSERT INTO document (`type_d`, `auteur_d`, `titre_d`) VALUES (?, ?, ?);");
-        stmt.setString(1, (String) args[0]);
-        stmt.setString(2, (String) args[1]);
-        stmt.setString(3, (String) args[2]);
-
-        stmt.execute();
-        stmt.close();
-        conn.close();
-    }
-
-    @Override
-    public int emprunterDocument(int numDocument, String nomUtilisateur) throws SQLException {
-        int id_u = getUserID(nomUtilisateur);
-
-        Connection conn = getConnection();
-        PreparedStatement stmt = conn.prepareStatement("UPDATE document SET `emprunt_d`=? WHERE `id_d`=?;");
-        stmt.setInt(1, id_u);
-        stmt.setInt(2, numDocument);
-
-        stmt.execute();
-        stmt.close();
-        conn.close();
-
-        return id_u;
-    }
-
-    @Override
-    public void retournerDocument(int numDocument) throws SQLException {
-        Connection conn = getConnection();
-        PreparedStatement stmt = conn.prepareStatement("UPDATE document SET `emprunt_d`=-1 WHERE `id_d`=?;");
-        stmt.setInt(1, numDocument);
-
-        stmt.execute();
-        stmt.close();
-        conn.close();
-    }
     
 }
